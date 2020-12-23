8771e2f8116f4adffe24c1628e3bdd4cf0c36fb4
Stefanos Mandalas <47466000+sealmove@users.noreply.github.com>
2020-12-23 02:09:27 +0200

    added conPos plugin

--- binarylang/plugins.nim
+++ binarylang/plugins.nim
@@ -13,6 +13,23 @@ template validPut*(field, encode, condition: untyped) =
   assert condition
   encode
 
+template condPosGet*(field, parse, condAndPos: untyped) =
+  let
+    (cond, pos) = condAndPos
+    save = getPosition(s)
+  if cond:
+    s.setPosition(pos)
+    parse
+    s.setPosition(save)
+template condPosPut*(field, encode, condAndPos: untyped) =
+  let
+    (cond, pos) = condAndPos
+    save = getPosition(s)
+  if cond:
+    s.setPosition(pos)
+    encode
+    s.setPosition(save)
+
 # Secondary (2 arguments)
 template validGet*(field, condition: untyped) =
   assert condition
